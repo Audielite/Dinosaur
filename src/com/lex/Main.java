980cdc8b35f95459bb028651cb2027a66382bd35
Cookie monster <emailemail.com>
2017-02-15 17:41:55 -0600

    Fixed version.

--- src/com/lex/Main.java
+++ src/com/lex/Main.java
@@ -3,12 +3,14 @@ package com.lex;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Dinosaur t_rex = new Dinosaur();
-        Dinosuaur velocoraptor = new Dinosaur():
+
+        Dino t_rex = new Dino();
+        Dino velocoraptor = new Dino();
+        Dino stegosaurus = new Dino();
 
         t_rex.roar();
         velocoraptor.roar();
+        stegosaurus.roar();
 
     }
 }
