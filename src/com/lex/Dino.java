6efc8008684c6a75c0615deaf226dce736b4c0e6
Cookie monster <emailemail.com>
2017-02-15 17:26:05 -0600

    Initial commit t_rex

---
+++ src/com/lex/Dino.java
@@ -0,0 +1,11 @@
+package com.lex;
+
+/**
+ * Created by alexishennings on 2/15/17.
+ */
+public class Dino {
+
+            public void roar() {
+                System.out.println("GRrR!!!");
+            }
+        }
---
+++ src/com/lex/Main.java
@@ -0,0 +1,14 @@
+package com.lex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dinosaur t_rex = new Dinosaur();
+        Dinosuaur velocoraptor = new Dinosaur():
+
+        t_rex.roar();
+        velocoraptor.roar();
+
+    }
+}
